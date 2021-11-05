d61a250020a45febfa912a7551069abb9df8c776
Dera <stephenie7533@gmail.com>
2021-11-05 16:14:13 +0100

    header worked on

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,10 +7,6 @@ class  Navbar extends Component {
     state = { clicked: false}
 
 
-    handleClick = () => {
-        {this.setState({ clicked : !this.state.clicked})}
-    }
-
     render() {
         return (
             <>
